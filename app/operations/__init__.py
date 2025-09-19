075fc80a4819f4b433c28545426bae971c80f7d6
Erica <ericaerath.music@gmail.com>
2025-09-19 12:25:09 -0400

    Added Operations Class and related methods

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,3 +1,28 @@
+#Define class 'operations'
+class Operations:
+
+#Copied old functions into operations class, added '@staticmethod' to define them as methods
+    @staticmethod
+    def addition(a: float, b: float) -> float:
+        return a + b
+
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+        return a - b
+
+    @staticmethod
+    def multiplication(a: float, b: float) -> float:
+        return a * b
+
+    @staticmethod
+    def division(a: float, b: float) -> float:
+        if b == 0:
+            raise ValueError("Division by zero is not allowed.")
+        return a / b
+
+
+#Commenting out old functions to keep them visible for reference
+"""
 def addition(a: float, b: float) -> float:
     return a + b
 
@@ -11,3 +36,4 @@ def division(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Division by zero is not allowed.")
     return a / b
+"""
